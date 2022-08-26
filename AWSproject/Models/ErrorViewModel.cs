c41cd80b65a0d176b36861d25b1e6ca65d78b520
Kusuma <Kusuma.puthalapattu@cognizant.com>
2022-08-26 20:03:13 +0530

    Add project files.

---
+++ AWSproject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AWSproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
